33ff48c82f9ecf6ac05c6be96e3d36fa4ee741da
KuanWang-Astro <kuw8@pitt.edu>
2022-09-13 23:22:04 -0400

    finished progenitor tree

--- halo_tree.py
+++ halo_tree.py
@@ -4,20 +4,27 @@ Module that constructs halo-based merger trees from the subhalo-based trees.
 """
 
 import numpy as np
-import h5py
-import os
 
-import load_by_file
+import simulation_box
 import locate_object
 import load_sublink
 
 
+def _groupnum_sn_1to2(groupsnapnum):
+    return [groupsnapnum % 1000000000000,
+            groupsnapnum // 1000000000000]
+
+def _groupnum_sn_2to1(groupnum, snapnum):
+    return snapnum * 1000000000000 + groupnum
+
+def load_fof_group(groupnum, snapnum, fields, sim):
+    pass
+
+
 def progenitor_halos(groupnum, snapnum, sim):
-    """ Loads the immediate progenitor halos (FOF groups) of the given halo.
-    Immediate progenitor halos are defined as halos that contain subhalos that
-    are immediate progenitors of the subhalos in the input halo. Note that in
-    some rare cases immediate progenitors are not in the immediately previous
-    snapshot.
+    """ Loads the progenitor halos (FOF groups) of the given halo. Progenitor
+    halos are defined as halos that contain subhalos that are progenitors of
+    the subhalos in the input halo.
 
     Parameters
     ----------
@@ -28,24 +35,55 @@ def progenitor_halos(groupnum, snapnum, sim):
 
     Returns
     -------
-
+    breadth-first.
 
     """
 
-    central = locate_object.subfind_central(groupnum, snapnum, sim)
-    subhalos = locat
-    progenitor_subhalos =
-    progenitor_halos =
+    central_sfid = locate_object.subfind_central(groupnum, snapnum, sim)
+    central_shid = locate_object.sublink_id(central_sfid, snapnum, sim)
+    fields = ['SubhaloID', 'DescendantID', 'LastProgenitorID',
+              'Group_M_TopHat200', 'SubhaloGrNr']
+    subhalos = load_sublink.load_group_subhalos(central_shid,
+                                                fields, sim)['SubhaloID']
+
+    progenitor_subhalos = load_sublink.merge_tree_dicts(
+                                 *[load_sublink.load_tree_progenitors(
+                                             subhalo, fields, sim,
+                                             main_branch_only = False)
+                                 for subhalo in subhalos],
+                                 fields = fields)
+
+    mask = (progenitor_subhalos['DescendantID'] != -1)
+    tree_halos = _groupnum_sn_2to1(*locate_object.group_num(
+               progenitor_subhalos['SubhaloID'][mask], sim))
+    descendant_pointers = _groupnum_sn_2to1(*locate_object.group_num(
+                     progenitor_subhalos['DescendantID'][mask], sim))
+    order = np.argsort(descendant_pointers)
+    descendant_pointers = descendant_pointers[order]
+    tree_halos = tree_halos[order]
+    breaks = np.append(np.where(np.roll(descendant_pointers, -1) !=
+                                descendant_pointers)[0],
+                       len(descendant_pointers))
+    halos_uniq = _groupnum_sn_1to2(descendant_pointers[breaks[:-1]])
+    halos_prog = np.array([_groupnum_sn_1to2(np.unique(
+                           tree_halos[breaks[i] : breaks[i + 1]]))
+                           for i in range(len(breaks) - 1)], dtype = object)
+
+    progenitor_halos_dict = {'Number': len(halos_uniq),
+                             'HaloGroupNum': halos_uniq[0],
+                             'HaloSnapNum': halos_uniq[1],
+                             'ProgenitorGroupNum': halos_prog[:, 0],
+                             'ProgenitorSnapNum': halos_prog[:, 1]}
+
+    return halos_uniq[0], halos_uniq[1], halos_prog[:, 0], halos_prog[:, 1]
 
 
-
+# data product -- all halo trees?
 
 
 def descendant_halos():
     pass
 
-def track_halo_history():
-    pass
 
-def construct_halo_tree():
+def construct_binary_halo_tree():
     pass
